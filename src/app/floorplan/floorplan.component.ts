6d23ff171e204a5879e4d74d6104f955473d7630
Andrej Dobak <andrej.dobak@gmail.com>
2017-05-09 11:28:21 +0200

    fixed name

--- src/app/floorplan/floorplan.component.ts
+++ src/app/floorplan/floorplan.component.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs/Observable';
 import { FloorplanService } from '../providers/floorplan.service';
 
 @Component({
-    templateUrl: 'Floorplan.component.html',
+    templateUrl: 'floorplan.component.html',
     providers: [ FloorplanService ]
 })
 
