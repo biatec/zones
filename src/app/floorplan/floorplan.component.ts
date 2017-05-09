e1fd4951cb6f2f2ceb8a3397090dbf9392c0d68e
Andrej Dobak <andrej.dobak@gmail.com>
2017-05-09 08:55:42 +0200

    Updated floorplan image

--- src/app/floorplan/floorplan.component.ts
+++ src/app/floorplan/floorplan.component.ts
@@ -15,8 +15,56 @@ export class FloorplanComponent implements OnInit {
     public z1class = '';
 
     private fp = [];
-    public zoneClass = {'AtriumZone1': 'zone-null', 'AtriumZone2': 'zone-null', 'AtriumZone3': 'zone-null', 'AtriumZone4': 'zone-null', 'MalaZone1': 'zone-null', 'MalaZone2': 'zone-null'};
+    public zoneClass = {'AtriumZone1': 'zone-null', 'AtriumZone2': 'zone-null', 'AtriumZone3': 'zone-null',
+                        'AtriumZone4': 'zone-null', 'MalaZone1': 'zone-null', 'MalaZone2': 'zone-null'};
     public zoneWifiClass = {'Mala2Zone1': 'zone-null'};
+
+    public socialChartData2: Array<any> = [
+    {
+      data: [1, 13, 9, 17, 34, 41, 38],
+      label: 'Utilization'
+    }
+    ];
+
+    public socialChartLabels: Array<any> = ['7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00',
+                                            '14:00', '15:00', '16:00', '17:00', '18:00', '19:00'];
+  public socialChartOptions: any = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      xAxes: [{
+        display: true,
+      }],
+      yAxes: [{
+        display: false,
+      }]
+    },
+    elements: {
+      line: {
+        borderWidth: 2
+      },
+      point: {
+        radius: 0,
+        hitRadius: 10,
+        hoverRadius: 4,
+        hoverBorderWidth: 3,
+      }
+    },
+    legend: {
+      display: false
+    }
+  };
+  public socialChartColours: Array<any> = [
+    {
+      backgroundColor: 'rgba(255,255,255,.2)',
+      borderColor: 'rgba(255,255,255,.55)',
+      pointBorderColor: 'rgba(255,255,255,.99)',
+      pointHoverBackgroundColor: '#fff'
+    }
+  ];
+  public socialChartLegend = false;
+  public socialChartType = 'line';
+
 
     constructor(private fpService: FloorplanService) {
 
